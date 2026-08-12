ce67e1220e33e185e9eab65cf1ac4894409c18c2
Devarth Darge <devarthdarge70@gmail.com>
2026-08-12 17:10:43 +0530

    Update default API URL for loan predictions

--- Frontend/loan_frontend/config.py
+++ Frontend/loan_frontend/config.py
@@ -2,7 +2,7 @@
 
 import os
 
-DEFAULT_API_URL = os.environ.get("LOAN_API_URL", "https://loan-approval-system-y2pg.onrender.com")
+DEFAULT_API_URL = os.environ.get("LOAN_API_URL", "https://loan-approval-system-y2pg.onrender.com/predict")
 
 STEPS = [
     "Loan Details",
